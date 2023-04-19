b2104ef49afe60747b32e23399e586456e68906d
Сергей Витальевич Королев <90028680+MikeMarshall36@users.noreply.github.com>
2023-04-19 17:42:33 +0300

    Набор текстов
    Здесь лежит словарь с названием текста и содержимым текста соответственно. Менять можно, но лучше добавлять. И да. Перенос делать через \t\n или enter+\t. Иначе не будет отображаться на странице должным образом

---
+++ Сайт/Example_texts.py
@@ -0,0 +1,86 @@
+Text_1 = """
+I read a fun fact about the brain\t
+And how it starts to deteriorate when\t
+We get to 27 or there abouts.\t
+It got me thinking about my head\t
+And what I can do to help stimulate it\t
+Makes me sad but...\t
+I forgot what I'm on about.\t
+\t
+Looked on the brightside, got keratitis.\t
+And you can't sit there until you're righteous.\t
+I wear a happy face like I'm Ed Gein.\t
+I feel all numb now, is that a feeling?\t
+Like all plastic boxed orange with no peel on.\t
+I wanna waste, I wanna waste, I wanna waste away.\t
+\t
+Alone getting high on a Saturday night\t
+I'm on the edge of a knife\t
+Nobody cares if I'm dead or alive\t
+Oh what a wonderful life\t
+"""
+
+Text_2 = """
+I'm a whisper lost upon wind\t
+I'm the ember that will burn you down\t
+I'm the water that will drown you\t
+I'm a star that's just a black hole now\t
+I'm a terrifying danger\t
+I'm fruit decaying on the ground\t
+I'm a swallower of anger\t
+I'm the tree that falls and makes no sound\t
+I make no sound...\t
+\t
+‘Cause if I stand up, I'll break my bones\t
+And everybody loves to see a fall unfold\t
+Ain't nobody giving up, 'cause nobody gives a fuck\t
+Stand up and break my bones\t
+Everybody wants what they just can't hold\t
+There's nobody praying for me\t
+"""
+
+Text_3 = """
+One thing, I don't know why\t
+It doesn't even matter how hard you try\t
+Keep that in mind, I designed this rhyme\t
+To explain in due time all I know\t
+Time is a valuable thing\t
+Watch it fly by as the pendulum swings\t
+Watch it count down to the end of the day\t
+The clock ticks life away, it's so unreal\t
+Didn't look out below\t
+Watch the time go right out the window\t
+Tryin' to hold on, didn't even know\t
+I wasted it all just to watch you go\t
+\t
+I kept everything inside\t
+And even though I tried, it all fell apart\t
+What it meant to me will eventually be\t
+A memory of a time when...\t
+\t
+I tried so hard and got so far\t
+But in the end it doesn't even matter\t
+I had to fall to lose it all\t
+But in the end it doesn't even matter\t
+"""
+
+Text_4 = """
+Space may be the final frontier\t
+But it's made in a Hollywood basement\t
+\t
+Cobain can you hear the spheres\t
+Singing songs off station to station\t
+\t
+And Alderon's not far away\t
+It's Californication\t
+\t
+Born and raised by those who praise\t
+Control of population\t
+\t
+everybody's been there\t
+And I don't mean on vacation\t
+First born unicorn\t
+
+"""
+
+songs_dict = {'Wonderful Life': Text_1, 'Nobody Praying For Me': Text_2, 'In The End': Text_3, 'Californication': Text_4}
