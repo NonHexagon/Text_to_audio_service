24668009d80132605c064f34fb2a179ae8e72648
Сергей Витальевич Королев <90028680+MikeMarshall36@users.noreply.github.com>
2023-04-26 17:35:09 +0300

    Строка + ошибки
    Добавлен ввод через строку и обработка ошибки с пустыми значениями 
    В приоритете файлы, а не строка

--- Сайт/application.py
+++ Сайт/application.py
@@ -1,15 +1,17 @@
 import os
 import time  # Импортируем модуль времени
 import random
+import threading
 import Example_texts
 from workwithpsswordandemail import send_message, generate_password
+from DB_manager import login_check
 from Example_texts import songs_dict
 from DataBase import Users, File, Session
 from pathlib import Path  # модуль для работы с путями, но нам нужен только инструмент для файлов
 from main import pdf_to_audio, clear_folder  # модуль для конвертации
 from flask import *  # модуль для создания веб-приложений
 from flask_sqlalchemy import SQLAlchemy  # пакет ORM СУБД
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 application = Flask(__name__)  # инициализация экземпляра класса веб-приложения на котором будем собирать проект
@@ -51,23 +53,24 @@ def about():  # обработчик пути
 def uploader():  # обработчик
     keys_arr = list(songs_dict.keys())
     s_name = str(random.choice(keys_arr)).title()
-    print(s_name)
-    text = songs_dict.get(s_name)
+    text_ = songs_dict.get(s_name)
     if request.method == 'POST':  # Проверка на запрос с методом POST
+        clear_folder('./files')
         file = request.files['file']  # получаем файл
-        file_name=file.filename
+        file_name = file.filename
         try:
             file.save(file_name)  # сохраняем полученный файл
         except FileNotFoundError:
             text = str(request.form['inp_text']).strip()  # Проверяем записи в текстовом поле
-            if text=="":  # Пустое поле
+            if text == "":  # Пустое поле
                 print('Empty input')
-                return render_template('uploader.html', message='Необходимо выбрать файл для озвучивания')
+                error = 'Файл для конвертации отсутствует или строка не была заполнена!'
+                return render_template('uploader.html', s_name=s_name, text=text_, error=error)
             else:
-                file_name='Text_to_audio_service'+str(time.time())+'.txt'  # создаём файл с текстом из поля
+                file_name = 'Text_to_audio_service'+str(time.time())+'.txt'  # создаём файл с текстом из поля
                 with open(f'./{file_name}', 'w') as f:
                     f.write(text)
-                print(f'Create file {file_name} for text from textblo')
+                print(f'Create file {file_name} for text from textblock')
         print(f'[&] {file_name}')  # вывод в консоль для отладки
         if str(Path(file_name).stem).title() in keys_arr:
             print('True')
@@ -80,8 +83,7 @@ def uploader():  # обработчик
             return send_file(f'files/{file_name}.mp3', as_attachment=True)  # Возврат получившегося файла
     if request.method == 'GET':  # Проверка запроса с методом GET
         clear_folder('./files')
-        return render_template('uploader.html', s_name=s_name, text=text)  # возвращаем страницу конвертора
-
+        return render_template('uploader.html', s_name=s_name, text=text_)  # возвращаем страницу конвертора
 
 
 @application.route('/register', methods=['POST', 'GET'])  # Объявление нужных методов
@@ -96,12 +98,16 @@ def registration():
         email = str(request.form['email']).strip()
         print(email)
         tmp_passwd = str(generate_password())  # пароль пока не используется
-        new_user = Users(user_name=user_name, f_name=f_name, l_name=l_name, email=email, tmp_passwd=tmp_passwd)
+        timestamp = str(datetime.now())
+        next_time = str(datetime.now() + timedelta(minutes=5))
+
+        new_user = Users(user_name=user_name, f_name=f_name, l_name=l_name, email=email, tmp_passwd=tmp_passwd,
+                         timestamp=timestamp, next_time=next_time)
         try:
             session_db = Session()  # Выполняем запись в бд
             session_db.add(new_user)
             session_db.commit()
-            send_message(email, tmp_passwd)
+            threading.Thread(target=(send_message(email, tmp_passwd)))
             print(f'Был добавлен новый пользователь: {l_name} {f_name} с ником {user_name}')
         except AttributeError:  # Обработка ошибки атрибутов (иногда случается)
             return 'Что-то пошло не так!'
@@ -115,6 +121,13 @@ def registration():
 def login():
     if request.method == 'GET':
         return render_template('login.html')
+    if request.method == 'POST':
+        input_passwd = str(request.form['tmp_passwd'])
+        input_email = str(request.form['email'])
+        if login_check(input_email, input_passwd):
+            return redirect('/uploader')
+        else:
+            return redirect('/register')
 
 
 if __name__ == '__main__':  # Создаем точку доступа
