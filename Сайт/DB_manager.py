e38485836731040bc4757ae6d5461bb0b176ad53
Сергей Витальевич Королев <90028680+MikeMarshall36@users.noreply.github.com>
2023-07-21 16:33:08 +0300

    Update DB_manager.py
    Перенос взаимодействия с бд на ОРМ + оптимизация некоторых функций

--- Сайт/DB_manager.py
+++ Сайт/DB_manager.py
@@ -1,12 +1,14 @@
+import select
 import sqlite3
 import threading
 import pandas as pd
-from DataBase import Users
+from sqlalchemy import *
+from DataBase import Users, engine, File, Session
 from datetime import datetime, timedelta
 from workwithpsswordandemail import generate_password, send_message
+
 DataBase = sqlite3.connect('main.db', check_same_thread=False)
 cursor = DataBase.cursor()
-
 
 
 def login_check(user_email: str, user_passwd: str) -> bool | str:
@@ -26,81 +28,118 @@ def login_check(user_email: str, user_passwd: str) -> bool | str:
     print('Что-то пошло не так')
 
 
+def data_extractor(req_column, where_state=False, com_stmt='', comp_to='', to_array=False):
+    result_arr = []
+    if where_state:
+        stmt = select(req_column).where(com_stmt == comp_to)
+        with engine.connect() as conn:
+            if to_array:
+                for row in conn.execute(stmt):
+                    result_arr.append([*row])
+                return result_arr
+            else:
+                for row in conn.execute(stmt):
+                    return row[0]
+    else:
+        stmt = select(req_column)
+        with engine.connect() as conn:
+            if to_array:
+                for row in conn.execute(stmt):
+                    result_arr.append([*row])
+                return result_arr
+            else:
+                for row in conn.execute(stmt):
+                    return row[0]
+
+
 def change_passwd():
     while True:
-        emails = cursor.execute("SELECT email FROM users").fetchall()
+        emails = data_extractor(Users.email, to_array=True)
         for email in emails:
+            user_name = data_extractor(Users.user_name, True, Users.email, email[0])
             passwd_1 = generate_password()
-            next_date = cursor.execute(f"SELECT next_time FROM users WHERE email == '{email[0]}'").fetchall()
-            if str(datetime.now()) >= str(next_date[0][0]):
-                cursor.execute(f"UPDATE users SET tmp_passwd = '{passwd_1}' WHERE email == '{email[0]}'")
-                cursor.execute(f"UPDATE users SET timestamp = next_time WHERE email == '{email[0]}'")
-                cursor.execute(f"UPDATE users SET next_time = '{datetime.now() + timedelta(minutes=120)}'\
-                WHERE email == '{email[0]}'")
-                DataBase.commit()
+            next_date = data_extractor(Users.next_time, True, Users.email, email[0])
+            if str(datetime.now()) >= str(next_date):
+                update_passwd = update(Users).where(Users.email == email[0]).values(tmp_passwd=passwd_1)
+                next_t = data_extractor(Users.next_time, True, Users.email, email[0])
+                update_stamp = update(Users).where(Users.email == email[0]).values(timestamp=next_t)
+                update_next = update(Users).where(Users.email == email[0]).values(
+                    next_time=(datetime.now() + timedelta(minutes=120)))
+
+                with Session.begin() as conn:
+                    conn.execute(update_passwd)
+                    conn.execute(update_stamp)
+                    conn.execute(update_next)
+
                 print(f'Пароль изменен и выслан на почту \033[33m{email[0]}\033[0m!\
                 \033[36mСледующее изменение в {datetime.now() + timedelta(minutes=120)}\033[0m')
-                send_message(email[0], passwd_1)
+                trunner = threading.Thread(target=send_message(email[0], passwd_1, user_name))
+                trunner.start()
+                trunner.join()
 
 
-def get_user_class_id(user_id: int) -> DataBase:
-    if user_id != 0:
-        _id = user_id
-        _user_name = cursor.execute(f"SELECT user_name FROM users WHERE id = {_id}").fetchone()
-        print('Имя пользователя: ', _user_name)
-        if _user_name:
-            _f_name = cursor.execute(f"SELECT f_name FROM users WHERE id = {_id}").fetchone()
-            _l_name = cursor.execute(f"SELECT l_name FROM users WHERE id = {_id}").fetchone()
-            _email = cursor.execute(f"SELECT email FROM users WHERE id = {_id}").fetchone()
-            _tmp_passwd = cursor.execute(f"SELECT tmp_passwd FROM users WHERE id = {_id}").fetchone()
-            _timestamp = cursor.execute(f"SELECT timestamp FROM users WHERE id = {_id}").fetchone()
-            _next_time = cursor.execute(f"SELECT next_time FROM users WHERE id = {_id}").fetchone()
+def get_user_class_uni(user_id=0, user_email=''):
+    prepared_data = []
+    needed_data_arr = [Users.id,
+                       Users.user_name,
+                       Users.f_name,
+                       Users.l_name,
+                       Users.tmp_passwd,
+                       Users.email,
+                       Users.timestamp,
+                       Users.next_time]
+    if user_id > 0:
+        user_email = ''
+        user_id = str(user_id)
 
-        else:
-            print('Ну нет такого!')
-            exit()
-        current_user = Users(id=_id, user_name=_user_name, f_name=_f_name, l_name=_l_name, email=_email,
-                             tmp_passwd=_tmp_passwd, timestamp=_timestamp, next_time=_next_time)
-        print(type(current_user))
-        return current_user
+        for data in needed_data_arr:
+            prepared_data.append(data_extractor(data, True, Users.id, user_id))
+        prepared_data = tuple(prepared_data)
+        print(prepared_data)
 
+    if len(user_email) > 0:
+        for data in needed_data_arr:
+            prepared_data.append(data_extractor(data, True, Users.email, user_email))
+        prepared_data = tuple(prepared_data)
+        print(prepared_data)
 
-def get_user_class_email(user_email: str) -> DataBase:
-    if user_email != '':
-        _id = cursor.execute(f"SELECT id FROM users WHERE email == '{user_email}'").fetchone()
-        if _id:
-            _user_name = cursor.execute(f"SELECT user_name FROM users WHERE email == '{user_email}'").fetchone()
-            _f_name = cursor.execute(f"SELECT f_name FROM users WHERE email == '{user_email}'").fetchone()
-            _l_name = cursor.execute(f"SELECT l_name FROM users WHERE email == '{user_email}'").fetchone()
-            _email = user_email
-            _tmp_passwd = cursor.execute(f"SELECT tmp_passwd FROM users WHERE email == '{user_email}'").fetchone()
-            _timestamp = cursor.execute(f"SELECT timestamp FROM users WHERE email == '{user_email}'").fetchone()
-            _next_time = cursor.execute(f"SELECT next_time FROM users WHERE email == '{user_email}'").fetchone()
-        else:
-            print('Ну нет такого!')
-            exit()
-        current_user = Users(id=_id, user_name=_user_name, f_name=_f_name, l_name=_l_name, email=_email,
-                             tmp_passwd=_tmp_passwd, timestamp=_timestamp, next_time=_next_time)
-        return current_user
+    _id, _user_name, _f_name, _l_name, _tmp_passwd, _email, _timestamp, _next_time = prepared_data
+
+    current_user = Users(id=_id,
+                         user_name=_user_name,
+                         f_name=_f_name,
+                         l_name=_l_name,
+                         email=_email,
+                         tmp_passwd=_tmp_passwd,
+                         timestamp=_timestamp,
+                         next_time=_next_time)
+    return current_user
 
 
-def reset_passwd(user_mail: str,):
+def reset_passwd(user_mail: str):
     mailboxes_query = cursor.execute("SELECT email FROM users").fetchall()
     mailboxes = []
     for i in range(len(mailboxes_query)):
         mailboxes.append(mailboxes_query[i][0])
     if user_mail in mailboxes:
-        print('in')
         passwd_1 = generate_password()
-        next_date = cursor.execute(f"SELECT next_time FROM users WHERE email == '{user_mail}'").fetchall()
-        cursor.execute(f"UPDATE users SET tmp_passwd = '{passwd_1}' WHERE email == '{user_mail}'")
-        cursor.execute(f"UPDATE users SET timestamp = next_time WHERE email == '{user_mail}'")
-        cursor.execute(f"UPDATE users SET next_time = '{datetime.now() + timedelta(minutes=120)}'\
-        WHERE email == '{user_mail}'")
-        DataBase.commit()
+        user_name = data_extractor(Users.user_name, True, Users.email, user_mail)
+
+        update_passwd = update(Users).where(Users.email == user_mail).values(tmp_passwd=passwd_1)
+        next_t = data_extractor(Users.next_time, True, Users.email, user_mail)
+        update_stamp = update(Users).where(Users.email == user_mail).values(timestamp=next_t)
+        print(datetime.now() + timedelta(minutes=120))
+        update_next = update(Users).where(Users.email == user_mail).values(
+            next_time=(datetime.now() + timedelta(minutes=120))
+        )
+        with Session.begin() as conn:
+            conn.execute(update_passwd)
+            conn.execute(update_stamp)
+            conn.execute(update_next)
+
         print(f'Пароль сброшен и выслан на почту \033[36m{user_mail}\033[0m!\
         \033[36mСледующее изменение в {datetime.now() + timedelta(minutes=120)}\033[0m')
-        trunner = threading.Thread(send_message(user_mail, passwd_1))
+        trunner = threading.Thread(target=send_message(user_mail, passwd_1, user_name))
         trunner.start()
         trunner.join()
     else:
@@ -108,37 +147,14 @@ def reset_passwd(user_mail: str,):
 
 
 def get_files(user_id):
-    user_files = cursor.execute(f"SELECT * FROM file WHERE file_owner == {user_id}").fetchall()
+    stmt = File.file_name, File.file_date
+    user_files = data_extractor(stmt, True, File.file_owner, user_id, to_array=True)
     df = pd.DataFrame(columns=['Название файла', 'Дата конвертации'])
-
     for i in range(len(user_files)):
-        df.loc[i] = [user_files[i][1]] + [user_files[i][2]]
+        df.loc[i] = [user_files[i][0]] + [user_files[i][1]]
     return df
 
 
-user_mails = cursor.execute("SELECT email FROM users").fetchall()
-
-
 if __name__ == '__main__':  # Создаем точку доступа
-    username_id = cursor.execute("SELECT user_name FROM users WHERE id == 1").fetchone()
-    files = cursor.execute("SELECT * FROM file").fetchall()
-    print(files)
-    print(username_id)
-    mail = str(input('Enter e-mail address: '))
-    users = cursor.execute("SELECT DISTINCT * FROM users;").fetchall()
-    files = cursor.execute("SELECT DISTINCT * FROM file;").fetchall()
-    passwd = cursor.execute(f"SELECT tmp_passwd FROM users WHERE email == '{mail}'").fetchall()
-    get_files(1)
-    print(user_mails)
-    for user in users:
-        print(f'Пользователь: {user}')
-    for file in files:
-        print(f'Файл: {file}')
-
-    print(*passwd)
-    print(login_check(mail, '1fQI2Tgx'))
-    get_user_class_email('mister22898@mail.ru')
-    get_user_class_id(1)
-    runner = threading.Thread(target=change_passwd())
-    runner.start()
-    runner.join()
+    stmt_2 = Users.email, Users.tmp_passwd, Users.id, Users.next_time, Users.timestamp
+    print(data_extractor(stmt_2, to_array=True))
