39d4578e37ba95939183dfe960fba0c33fcac832
Сергей Витальевич Королев <90028680+MikeMarshall36@users.noreply.github.com>
2023-07-21 16:36:12 +0300

    Update workwithpsswordandemail.py
    Ну типа оптимизация (просто убрал некоторые комментарии)

--- Сайт/workwithpsswordandemail.py
+++ Сайт/workwithpsswordandemail.py
@@ -4,24 +4,15 @@ import sqlite3
 import smtplib
 from email.mime.text import MIMEText
 from random import choice
-from DataBase import get_user_name
 import ssl
 from keys import _mail, _passwd
 
 
-# from datetime import datetime  #для времени создания пароля
-# now = datetime.now()
-def send_message(mail, text):  # Отправка сообщения пользователю. Адрес получателя, пароль
+def send_message(mail, text, user_name):  # Отправка сообщения пользователю. Адрес получателя, пароль
     db = sqlite3.connect('main.db')
     cursor = db.cursor()
-    name = cursor.execute(f"SELECT DISTINCT f_name FROM users WHERE email == '{mail}'").fetchall()
-    user_name = cursor.execute(f"SELECT DISTINCT user_name FROM users WHERE email == '{mail}'").fetchall()
-    # текст сообщения
-    # name = name[0][0]
-    # user_name = user_name[0][0]
-
     our_mail = str(_mail)  # наша почта
-    message = MIMEText('{user_name} Here is your temperate password: {text} '.format(user_name=f"{get_user_name(mail)}",
+    message = MIMEText('{user_name} Here is your temperate password: {text} '.format(user_name=f"{user_name}",
                                                                                      text=text))
     message['From'] = our_mail
     message['To'] = mail
@@ -43,10 +34,6 @@ def check_email(email):
         return False
 
 
-# альт вариант. Согласовать
-# from validate_email import validate_email
-# valid_email = validate_email(email_address='example@gmail.com', check_smtp = True)
-
 # генерация пароля
 def generate_password():
     simbols = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
@@ -59,3 +46,4 @@ def generate_password():
 if __name__ == '__main__':  # Создаем точку доступа
     print(generate_password())
     send_message('204543@edu.fa.ru', 'Hello there, you filthy bastard!')
+
